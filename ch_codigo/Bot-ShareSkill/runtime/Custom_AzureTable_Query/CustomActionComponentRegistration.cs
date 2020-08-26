d3d51f79471c813deb52e440a52c7a92d4ac60ba
ChaimCG <47600169+ChaimCG@users.noreply.github.com>
2020-08-26 15:31:19 -0500

    chaim master

---
+++ ch_codigo/Bot-ShareSkill/runtime/Custom_AzureTable_Query/CustomActionComponentRegistration.cs
@@ -0,0 +1,23 @@
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs.Debugging;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Microsoft.BotFramework.Composer.CustomAction_AzureTable_Query
+{
+    public class CustomComponentRegistration_AzureTable_Query : ComponentRegistration, IComponentDeclarativeTypes
+    {
+        public IEnumerable<DeclarativeType> GetDeclarativeTypes(ResourceExplorer resourceExplorer)
+        {
+            // Actions
+            yield return new DeclarativeType<AzureTable_Query>(AzureTable_Query.Kind);
+        }
+
+        public IEnumerable<JsonConverter> GetConverters(ResourceExplorer resourceExplorer, SourceContext sourceContext)
+        {
+            yield break;
+        }
+    }
+}
