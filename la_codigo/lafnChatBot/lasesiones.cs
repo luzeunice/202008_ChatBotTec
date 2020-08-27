3e469c25964abd4a3c01412307eb14605075aacb
LuzEuniceTec <53527436+LuzEuniceTec@users.noreply.github.com>
2020-08-26 21:15:31 -0500

    Agregar Key Vault Azure ( lasesiones / lausuarios)

--- la_codigo/lafnChatBot/lasesiones.cs
+++ la_codigo/lafnChatBot/lasesiones.cs
@@ -1,3 +1,21 @@
+/***************************************************
+* FUNCION PARA OBTENER 3 SESIONES SIGUIENTES
+* 
+* ENTRADA: 
+* Ninguna
+* 
+* SALIDA: 
+* Resultado1 : Detalle del evento1
+* Resultado2 : Detalle del evento2
+* Resultado3 : Detalle del evento3
+* 
+* VALIDACIÓN: 
+* Si no hay información en la sesión: Colocar nulls
+* 
+* Creado por: Luz Eunice Angeles Ochoa 
+* Fecha: 26 de agosto de 2020
+*******************************************************/
+
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -8,13 +26,16 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-
 using Microsoft.Azure.Cosmos.Table;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using CosmosTableSamples.Model;
 
+
+using Azure.Identity;
+using Azure.Core;
+using Azure.Security.KeyVault.Secrets;
 
 
 
@@ -35,22 +56,55 @@ namespace lafnChatBot
 
             }
 
-            if ((string)req.Method == "POST")
+            /****************************
+            * GET
+            ****************************/
+            if ((string)req.Method == "GET")
             {
                 log.LogInformation("C# HTTP trigger function processed a request POST.");
+
+                /* **************************
+                 * Información no necesaria
+                 
                 identificador = req.Query["identificador"];
 
                 string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
                 dynamic data = JsonConvert.DeserializeObject(requestBody);
                 identificador = identificador ?? data?.identificador;
+                ****************************/
 
+                /* **************************
+                * Conexion Key Vault 
+                Referencia: https://docs.microsoft.com/en-us/azure/key-vault/general/tutorial-net-virtual-machine
+                ****************************/
 
-                var account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=stg0la0tablas;AccountKey=M0GS+KDCUELQ5UbopjaHSy8l89+s8bolqtja5hWmQOdzykCifXJLpaC2E0MVvIdsWSdtWrjkWiZRmVe0xQldTw==;EndpointSuffix=core.windows.net");
-                var client = account.CreateCloudTableClient();
-                var tableUsuarios = client.GetTableReference("la0sesiones");
+                SecretClientOptions options = new SecretClientOptions()
+                {
+                    Retry =
+                    {
+                        Delay= TimeSpan.FromSeconds(2),
+                        MaxDelay = TimeSpan.FromSeconds(16),
+                        MaxRetries = 5,
+                        Mode = RetryMode.Exponential
+                    }
+                };
 
-                List<Sesion> sesiones = tableUsuarios.CreateQuery<Sesion>().AsQueryable<Sesion>().Where(e => e.PartitionKey == "Sesiones" && e.fecha_evento>= DateTime.UtcNow ).ToList();
+                var client = new SecretClient(new Uri("https://kevaultchatbot.vault.azure.net/"), new DefaultAzureCredential(), options);
+                KeyVaultSecret secret = client.GetSecret("storageTablas");
+                string secretValue = secret.Value;
+                CloudStorageAccount storageAccount;
+                storageAccount = CloudStorageAccount.Parse(secretValue);
+                CloudTableClient tableClient = storageAccount.CreateCloudTableClient(new TableClientConfiguration());
+                CloudTable tableSesiones = tableClient.GetTableReference("la0sesiones");
+
+                /****************************
+                * OBTENER LA INFORMACIÓN 
+                ****************************/
+
+                List<Sesion> sesiones = tableSesiones.CreateQuery<Sesion>().AsQueryable<Sesion>().Where(e => e.PartitionKey == "Sesiones" && e.fecha_evento>= DateTime.UtcNow ).ToList();
                 List<Sesion> sesiones_orden = sesiones.OrderBy(c => c.fecha_evento).ToList();
+
+                var sesionVacia = new List<Sesion>() { new Sesion() { titulo = "Sin Próximas Sesiones Planeadas" } };
 
                 if (sesiones.Count >= 3 )
                 {
@@ -61,18 +115,23 @@ namespace lafnChatBot
 
                 if (sesiones.Count == 2)
                 {
-                    string responseMessageS = JsonConvert.SerializeObject(new { Resultado1 = sesiones_orden[0], Resultado2 = sesiones_orden[0] });
+                    string responseMessageS = JsonConvert.SerializeObject(new { Resultado1 = sesiones_orden[0], Resultado2 = sesiones_orden[1], Resultado3 = sesionVacia[0] });
 
                     return new OkObjectResult(responseMessageS);
                 }
 
                 if (sesiones.Count == 1)
                 {
-                    string responseMessageS = JsonConvert.SerializeObject(new { Resultado1 = sesiones_orden[0] });
+                    string responseMessageS = JsonConvert.SerializeObject(new { Resultado1 = sesiones_orden[0], Resultado2 = sesionVacia[0], Resultado3 = sesionVacia[0] });
 
                     return new OkObjectResult(responseMessageS);
                 }
+                else
+                {
+                    string responseMessageS = JsonConvert.SerializeObject(new { Resultado1 = sesionVacia[0], Resultado2 = sesionVacia[0], Resultado3 = sesionVacia[0] });
 
+                    return new OkObjectResult(responseMessageS);
+                }
 
 
             }
--- la_codigo/lafnChatBot/lausuarios.cs
+++ la_codigo/lafnChatBot/lausuarios.cs
@@ -1,3 +1,20 @@
+
+/****************************
+* FUNCION PARA CONSULTAR LA INFORMACIÓN DE USUARIO
+* 
+* ENTRADA: 
+* Identificador ( Nomina o correo) 
+* 
+* SALIDA: 
+* Resultado: Información del usuario como nombre, nomina, correo, etc. 
+* 
+* VALIDACIÓN: 
+* Si no encuentra información de usuario colocar en nombre: "Sin Datos"
+* 
+* Creado por: Luz Eunice Angeles Ochoa 
+* Fecha: 26 de agosto de 2020
+****************************/
+
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -8,7 +25,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-
 using Microsoft.Azure.Cosmos.Table;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +32,9 @@ using System.Net;
 using CosmosTableSamples.Model;
 
 
+using Azure.Identity;
+using Azure.Core;
+using Azure.Security.KeyVault.Secrets;
 
 
 namespace lafnChatBot
@@ -35,33 +54,71 @@ namespace lafnChatBot
 
             }
 
-            if ((string)req.Method == "POST")
+            /****************************
+            * GET
+            ****************************/
+            if ((string)req.Method == "GET")
             {
-                log.LogInformation("C# HTTP trigger function processed a request GET.");
-                identificador = req.Query["identificador"];
 
+
+                log.LogInformation("C# HTTP trigger function processed a request GET.");
+                
+                /* **************************
+                 * Identificador 
+                 ****************************/ 
+
+                identificador = req.Query["identificador"];
                 string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
                 dynamic data = JsonConvert.DeserializeObject(requestBody);
                 identificador = identificador ?? data?.identificador;
 
+                /* **************************
+                 * Conexion Key Vault 
+                 Referencia: https://docs.microsoft.com/en-us/azure/key-vault/general/tutorial-net-virtual-machine
+                 ****************************/
+                SecretClientOptions options = new SecretClientOptions()
+                {
+                    Retry =
+                    {
+                        Delay= TimeSpan.FromSeconds(2),
+                        MaxDelay = TimeSpan.FromSeconds(16),
+                        MaxRetries = 5,
+                        Mode = RetryMode.Exponential
+                    }
+                };
 
-                var account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=stg0la0tablas;AccountKey=M0GS+KDCUELQ5UbopjaHSy8l89+s8bolqtja5hWmQOdzykCifXJLpaC2E0MVvIdsWSdtWrjkWiZRmVe0xQldTw==;EndpointSuffix=core.windows.net");
-                var client = account.CreateCloudTableClient();
-                var tableUsuarios = client.GetTableReference("la0usuario");
+                var client = new SecretClient(new Uri("https://kevaultchatbot.vault.azure.net/"), new DefaultAzureCredential(), options);
+                KeyVaultSecret secret = client.GetSecret("storageTablas");
+                string secretValue = secret.Value;
+                CloudStorageAccount storageAccount;
+                storageAccount = CloudStorageAccount.Parse(secretValue);
+                CloudTableClient tableClient = storageAccount.CreateCloudTableClient(new TableClientConfiguration());
 
+
+                /* **************************
+                 * Conexion a la tabla de usuarios 
+                * **************************/
+
+                CloudTable tableUsuarios = tableClient.GetTableReference("la0usuario");
                 List<Usuario> usuario = tableUsuarios.CreateQuery<Usuario>().AsQueryable<Usuario>().Where(e => e.PartitionKey == "Empleado" && ( e.nomina == identificador || e.correo == identificador)).ToList();
-                
+               
+                var usuarioVacio = new List<Usuario>() { new Usuario() { nombre = "Sin Datos" } };
+
+                /* **************************
+                * Si tiene registros regresar la información , sino colocar en el nombre="Sin Datos"
+                 **************************/
 
                 if (usuario.Count >= 1 )
                 {
                         return new OkObjectResult(JsonConvert.SerializeObject(new { Resultado = usuario[0] }));
                 }
-
+                else 
+                {
+                        return new OkObjectResult(JsonConvert.SerializeObject(new { Resultado = usuarioVacio[0] })); 
+                }
 
                 }
-            string responseMessage = string.IsNullOrEmpty(identificador)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {(string)req.Method}. This HTTP triggered function executed successfully.";
+            string responseMessage = "OK";
 
             return new OkObjectResult(responseMessage);
         }
